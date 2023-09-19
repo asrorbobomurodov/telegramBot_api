bf6f41a86f9ef38fef92bc55cfe467b3e90fd068
JavohirJalilov <javohirjaliloff@gmail.com>
2023-09-19 18:39:43 +0500

    sendmessage

--- sendMessage.py
+++ sendMessage.py
@@ -1,9 +1,19 @@
 import os
 import requests
 
-TOKEN = ''
+TOKEN = '6388893107:AAGIbsoNExuuEgRirNnCbLQyN_wI9-9BrPA'
 
 def sendMessage(chat_id:str, text:str):
+    params = {
+        "chat_id":chat_id,
+        "text": text
+    }
     URL = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+
+    response = requests.get(URL, params=params)
+
+    return response.status_code
     
-    
+chat_id = 5575549228
+text = "hi"
+print(sendMessage(chat_id, text))
