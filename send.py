6639f9784d4a346459f5dd6c3ae8c04b4d6db3cf
JavohirJalilov <javohirjaliloff@gmail.com>
2023-09-26 18:20:48 +0500

    send docuemnt with file id

--- send.py
+++ send.py
@@ -6,17 +6,15 @@ TOKEN = '6388893107:AAHm55DuPheZmctXz5mX5HnSfD9zfgmpNhY'
 
 def sendDocument(chat_id, document):
     params = {
-        "chat_id": chat_id
-    }
-    file = {
+        "chat_id": chat_id,
         "document": document
     }
     URL = f'https://api.telegram.org/bot{TOKEN}/sendDocument'
 
-    response = requests.post(URL, params=params, files=file)
+    response = requests.post(URL, params=params)
     return response.json()
 
-document = open('requirements.txt', 'r')
+document = 'BQACAgIAAxkBAAIBO2US2m3ZhtnV_yKll4mOna1ftX3wAALBNAAClx2QSMymeSz6F6twMAQ'
 
 data = get_updates(TOKEN)
 chat_id = data['message']['chat']['id']
