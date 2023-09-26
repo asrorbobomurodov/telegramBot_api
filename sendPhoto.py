3e162c3f8b33afe9d1bcb8a0511d9d8f0a284257
JavohirJalilov <javohirjaliloff@gmail.com>
2023-09-26 18:12:02 +0500

    send photo with file

--- sendPhoto.py
+++ sendPhoto.py
@@ -6,18 +6,23 @@ TOKEN = '6388893107:AAHm55DuPheZmctXz5mX5HnSfD9zfgmpNhY'
 def sendPhoto(chat_id:str,photo:str):
     params = {
         "chat_id": chat_id,
-        "photo": photo
+    }
+    file = {
+        "photo":photo
     }
     URL = f'https://api.telegram.org/bot{TOKEN}/sendPhoto'
 
-    response = requests.get(URL, params = params)
+    response = requests.post(URL, params = params, files=file)
     return response.json()
 
 
 # photo_url='https://miro.medium.com/v2/resize:fit:300/1*YVTFl1UEkt3_rkez-DIU9w.png'
-file_id = 'AgACAgIAAxkBAAIBNGUS0WLd7IBg9Dt6kh8UDlG-zg-PAAL6zDEblx2QSM_jdc2XZAi1AQADAgADcwADMAQ'
+# file_id = 'AgACAgIAAxkBAAIBNGUS0WLd7IBg9Dt6kh8UDlG-zg-PAAL6zDEblx2QSM_jdc2XZAi1AQADAgADcwADMAQ'
+photo = open('bot.png', 'rb')
+
 data = get_updates(TOKEN)
 chat_id = data['message']['chat']['id']
 
+
 # send photo with three ways: url, id, file
-sendPhoto(chat_id, file_id)
+sendPhoto(chat_id, photo)
