2b51377e49524a5954849fef0fbc6d4590a2b530
JavohirJalilov <javohirjaliloff@gmail.com>
2023-09-28 18:48:15 +0500

    send keyboard

--- sendKeyboard.py
+++ sendKeyboard.py
@@ -1,8 +1,33 @@
 import os
 import requests
 
-TOKEN = ''
+TOKEN='6388893107:AAHm55DuPheZmctXz5mX5HnSfD9zfgmpNhY'
 
 def sendMessage(chat_id:str, text:str):
-    URL = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+    button1 = {
+        "text": "Button1",
+        "request_contact": True
+    }
+    button2 = {
+        "text": "Button2"
+    }
+    button3 = {
+        "text": "Button3"
+    }
+    keyboard = {"keyboard": [[button1, button2], [button3]], "resize_keyboard": True}
+
+    params = {
+        "chat_id":chat_id,
+        "text": text,
+        "reply_markup": keyboard
+    }
     
+    url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+
+    response = requests.get(url, json=params)
+
+    return response.json()
+
+chat_id = 5575549228
+text = "hi"
+print(sendMessage(chat_id, text))
