56a4f0b4c5e04fea87e0a4be5980312597d3adad
asrorbobomurodov <mr.aceonejr777@gmail.com>
2023-10-17 07:41:11 +0500

    send echo Photo

--- sendPhoto.py
+++ sendPhoto.py
@@ -1,23 +1,19 @@
-import os 
 import requests
 from getUpdates import get_updates
-TOKEN = '6388893107:AAHm55DuPheZmctXz5mX5HnSfD9zfgmpNhY'
+TOKEN = 'Token here'
 
-def sendPhoto(chat_id:str,photo:str):
+def sendPhoto(chat_id:str, photo:str):
     params = {
         "chat_id": chat_id,
     }
     file = {
-        "photo":photo
+        "photo": photo
     }
     URL = f'https://api.telegram.org/bot{TOKEN}/sendPhoto'
 
     response = requests.post(URL, params = params, files=file)
     return response.json()
 
-
-# photo_url='https://miro.medium.com/v2/resize:fit:300/1*YVTFl1UEkt3_rkez-DIU9w.png'
-# file_id = 'AgACAgIAAxkBAAIBNGUS0WLd7IBg9Dt6kh8UDlG-zg-PAAL6zDEblx2QSM_jdc2XZAi1AQADAgADcwADMAQ'
 photo = open('bot.png', 'rb')
 
 data = get_updates(TOKEN)
